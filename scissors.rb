c7b75ae8e8af24bda993d91f08a64f79037e3c13
idleberg <jan@idleberg.com>
2014-09-20 00:47:36 +0200

    added option to delete completions afterwards

--- scissors.rb
+++ scissors.rb
@@ -5,14 +5,18 @@ require "builder"
 require "oj"
 
 # Configuration
-$scope           = "text.html"
-$replace_strings = [
+$set_scope          = "text.html"
+$to_subfolder       = true
+$delete_completions = false
+
+# Replace string in trigger before creating file
+$replace_strings   = [
     [" ",   "_"],
     ["\t",  "-"]
 ]
 
 meta_info = <<-EOF
-\nsublime-scissors, version 0.0.1
+\nsublime-scissors, version 0.0.2
 The MIT License
 Copyright (c) 2014 Jan T. Sott
 EOF
@@ -24,18 +28,18 @@ def product_xml(trigger, contents)
     xml.snippet do |el|
         el << "  <content><![CDATA[\n"+contents+"\n]]><content>\n"
         el.tabTrigger trigger
-        el.scope $scope
+        el.scope $set_scope
     end
 end
 
 puts meta_info
 
 # Iterate over completions in current directory
-Dir.glob("*.sublime-completions") do |file|
+Dir.glob("*.sublime-completions") do |completions|
 
-    puts "\n<< Reading \"#{file}\""
+    puts "\n<< Reading \"#{completions}\""
 
-    json = File.read(file)
+    json = File.read(completions)
     parsed = Oj.load(json)
 
     # Iterate over completions in JSON
@@ -51,13 +55,31 @@ Dir.glob("*.sublime-completions") do |file|
             trigger = trigger.to_s.gsub(needle, replacement)
         end
 
+        # Set target directory
+        if $to_subfolder == true
+            dir = File.basename(completions, ".*")
+
+            unless Dir.exists?(dir)
+                Dir.mkdir(dir)
+            end
+        else
+            dir = "."
+        end
+
         # Write snippets
-        File.open("#{trigger}.sublime-snippet", "w") do |snippet|
+        File.open("#{dir}/#{trigger}.sublime-snippet", "w") do |snippet|
           puts ">> Writing \"#{trigger}.sublime-snippet\""
           snippet.write(product_xml(trigger, contents))   
         end
     end
 
-    # Game Over
-    puts "\nCompleted."
+    # Delete completions
+    if $delete_completions == true
+        puts "^^ Deleting \"#{completions}\""
+        File.delete(completions)
+    end
+
 end
+
+# Game Over
+puts "\nCompleted."
